e17fbde2bfe905d3eeb25d3d0c545f07d19e27fe
Manash Roy <manashroy454@gmail.com>
2023-12-27 09:30:33 +0530

    Updated Solution.java

--- Solution.java
+++ Solution.java
@@ -1,13 +1,12 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Solution{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int arr[] = new int[n];
-        for(int i=0;i<n;i++) arr[i] = sc.nextInt();
-        for(int i=0;i<n;i++){
-            System.out.println(arr[i]);
-        }
+        int arr[] = new int[n]; // declearing array
+        for(int i=0;i<n;i++) arr[i] = sc.nextInt(); // taking array input
+        for(int i=0;i<n;i++) System.out.println(arr[i]); // printing array input
+        sc.close(); 
     }
 }
